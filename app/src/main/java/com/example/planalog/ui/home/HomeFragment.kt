f95d665a91e22deb802a8be9d8767cb539f1e744
hdg0116 <hdg0116@gmail.com>
2025-01-14 04:31:37 +0900

    Feat: 홈화면 UI 및 플래너 기능 구현(#7)

--- app/src/main/java/com/example/planalog/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/planalog/ui/home/HomeFragment.kt
@@ -5,10 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.planalog.databinding.FragmentHomeBinding
+import com.example.planalog.ui.home.ctgy.Category
+import com.example.planalog.ui.home.ctgy.CategoryAdapter
+import com.example.planalog.ui.home.ctgy.MemoAdapter
+import com.example.planalog.ui.home.memo.ChecklistItem
+import com.example.planalog.utils.generateRandomColor
 
 class HomeFragment : Fragment() {
-    lateinit var binding : FragmentHomeBinding
+
+    private lateinit var binding : FragmentHomeBinding
+
+    private val checklist = mutableListOf<ChecklistItem>()
+    private val categories = mutableListOf<Category>()
+    private lateinit var ctgyAdapter : CategoryAdapter
+    private lateinit var memoAdapter: MemoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +30,44 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // RecyclerView 설정
+        ctgyAdapter = CategoryAdapter(categories)
+        binding.homePlannerCtgyRv.layoutManager = LinearLayoutManager(context)
+        binding.homePlannerCtgyRv.adapter = ctgyAdapter
+
+        memoAdapter = MemoAdapter(checklist)
+        binding.homePlannerMemoRv.layoutManager = LinearLayoutManager(context)
+        binding.homePlannerMemoRv.adapter = memoAdapter
+
+        // 초기 카테고리 추가
+        if (categories.isEmpty()) {
+            addCategory("") // 기본 카테고리 제목 설정
+        }
+
+        // Tools 버튼: 카테고리 추가
+        binding.homePlannerCtgyToolsIc.setOnClickListener {
+            addCategory("")
+        }
+
+        // 메모형 체크리스트 추가 버튼 클릭 리스너
+        binding.homePlannerMemoPlusIc.setOnClickListener {
+            addCheckListItem("")
+        }
+    }
+
+    private fun addCheckListItem(task: String) {
+        checklist.add(ChecklistItem(task, true))
+        memoAdapter.notifyItemInserted(checklist.size - 1)
+    }
+
+    private fun addCategory(title: String) {
+        val color = generateRandomColor()
+        categories.add(Category(title, mutableListOf(), color))
+        ctgyAdapter.notifyItemInserted(categories.size - 1)
     }
 }
